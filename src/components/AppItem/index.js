2150892f41d16bbed0a9b8714946adc3794715e6
arunmacs <aruncharymuniganti@gmail.com>
2021-06-26 13:20:19 +0000

    added style for tabs and refreshed

--- src/components/AppItem/index.js
+++ src/components/AppItem/index.js
@@ -8,6 +8,11 @@ const AppItem = props => {
     <li className="app-container">
       <img src={imageUrl} className="app-image" alt="app-pic" />
       <p className="app-name">{appName}</p>
+      <a href="https://play.google.com/store" target="_blank" rel="noreferrer">
+        <button type="button" className="install-btn">
+          Install
+        </button>
+      </a>
     </li>
   )
 }
